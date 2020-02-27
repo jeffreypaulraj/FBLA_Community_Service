1caf833421758b126cdf7811be50c934e9cea8a1
Jeffrey Paulraj <jeffrey.paulraj@gmail.com>
2020-02-26 20:56:44 -0500

    sign in page, retrieve data from db, report page

--- fblaproj/venv/fbla_project.py
+++ fblaproj/venv/fbla_project.py
@@ -3,6 +3,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+import pyrebase
+
 app = Flask(__name__)
 
 cred = credentials.Certificate("/Users/Jeffrey/Downloads/sbfbla-2d88f-firebase-adminsdk-9qpc2-d9b6f84a5a.json")
@@ -13,6 +15,21 @@ root = db.reference()
 user = db.reference()
 
 idNumber = None
+
+config = {
+    "apiKey": "AIzaSyByziQa01XAmoShhQjglWtQ6-BUHwkn61M",
+    "authDomain": "sbfbla-2d88f.firebaseapp.com",
+    "databaseURL": "https://sbfbla-2d88f.firebaseio.com",
+    "projectId" : "sbfbla-2d88f",
+    "storageBucket": "sbfbla-2d88f.appspot.com",
+    "messagingSenderId": "391063470721",
+    "appId": "1:391063470721:web:a6a9341c42b0224702c380",
+    "measurementId": "G-2GLLKDXD3W"
+}
+
+firebase_auth_setup = pyrebase.initialize_app(config)
+
+auth = firebase_auth_setup.auth()
 
 @app.route('/')
 def index():
@@ -28,26 +45,71 @@ def signup():
 
 @app.route('/signupform', methods=['POST'])
 def signupForm():
-    user = root.child(request.form.get('idnumber')).set({
+    print(request.form.get('idnumber'))
+    user = root.child('Users').child(request.form.get('idnumber')).set({
         'Name': request.form.get('username'),
         'Id Number': request.form.get('idnumber') ,
         'Grade': request.form.get('grade'),
-        'Total Hours': 0
+        'Total Hours': 0,
+        'Total Entries': 0
     })
-    print('Name: ' + request.form.get('username'))
     idNumber = request.form.get('idnumber')
     name = request.form.get('username')
-    return render_template("index.html", idNumber = idNumber, name = name)
+    email = request.form.get('emailaddress')
+    password = request.form.get('password')
+    user_auth = auth.create_user_with_email_and_password(email, password)
+    total_entries = 0
+    total_hours = 0
+    user_db = db.reference('Users')
+    max_hours_user = user_db.order_by_child('Total Hours').limit_to_last(1).get()
+    max_hours_user_id = next(iter(max_hours_user))
+    max_hours = root.child('Users').child(max_hours_user_id).child('Total Hours').get()
+    return render_template("index.html", idNumber = idNumber, name = name, total_entries = total_entries, total_hours = 0, max_hours = max_hours)
+
+@app.route('/signinform', methods=['POST'])
+def signinForm():
+    email = request.form.get('emailaddress')
+    password = request.form.get('password')
+    idNumber = email[0:8]
+    print(idNumber)
+    user_auth = auth.sign_in_with_email_and_password(email, password)
+    idNumber = root.child('Users').child(idNumber).child('Id Number').get()
+    name = root.child('Users').child(idNumber).child('Name').get()
+    total_entries = root.child('Users').child(idNumber).child('Total Entries').get()
+    total_hours = root.child('Users').child(idNumber).child('Total Hours').get()
+    user_db = db.reference('Users')
+    max_hours_user = user_db.order_by_child('Total Hours').limit_to_last(1).get()
+    max_hours_user_id = next(iter(max_hours_user))
+    max_hours = root.child('Users').child(max_hours_user_id).child('Total Hours').get()
+    return render_template("index.html", idNumber = idNumber, name = name, total_entries = total_entries, total_hours = total_hours, max_hours = max_hours)
 
 @app.route('/processHours', methods=['POST'])
 def processHours():
-    hours_entered = request.form.get('hoursEntered')
+    hours_entered = int(request.form.get('hoursEntered'))
     description_entered = request.form.get('descriptionEntered')
     idNumber = request.form.get('idNumber')
-    current_hours = user.child(idNumber).get('Total Hours')
+    current_hours = root.child('Users').child(idNumber).child('Total Hours').get()
+    current_entries = root.child('Users').child(idNumber).child('Total Entries').get()
+    total_entries = current_entries + 1
     name = request.form.get('name')
-    user.child(idNumber).update({'Total Hours': current_hours + hours_entered})
-    return render_template("index.html", idNumber = idNumber, name = name)
+    print(type(hours_entered))
+    root.child('Users').child(idNumber).update({'Total Hours': current_hours + hours_entered})
+    root.child('Users').child(idNumber).update({'Total Entries': total_entries})
+    total_hours = root.child('Users').child(idNumber).child('Total Hours').get()
+    user_db = db.reference('Users')
+    max_hours_user = user_db.order_by_child('Total Hours').limit_to_last(1).get()
+    max_hours_user_id = next(iter(max_hours_user))
+    max_hours = root.child('Users').child(max_hours_user_id).child('Total Hours').get()
+    print('Max Hours:')
+    return render_template("index.html", idNumber = idNumber, name = name, total_entries = total_entries, total_hours = total_hours, max_hours = max_hours)
+
+
+@app.route('/reportHours', methods=['GET'])
+def reportHours():
+    user_db = db.reference('Users')
+    all_users = user_db.order_by_child('Grade').get()
+    return render_template("report.html", all_data = all_users)
+
 
 @app.route('/home')
 def home():
